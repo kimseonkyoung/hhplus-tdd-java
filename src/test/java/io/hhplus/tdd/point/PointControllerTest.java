9210293fbc4b56b7ba5292161577cb6b31c3f7a8
Kimseonkyoung <kwswaver@gmail.com>
2024-12-19 01:21:57 +0900

    Test: [ControllerTest] 특정 유저의 포인트 조회 시 반환값 로직 추가에 따른 테스트 수정 - 반환값이 올바르게 반환되는지 검증하는 테스트 수정 - Mock 설정: given() willReturn() 사용

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,13 +1,16 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.UserNotFoundException;
+import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,17 +22,22 @@ public class PointControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PointService pointService;
+
     @Test
     @DisplayName("/point 요청시 특정 유저의 포인트를 조회한다.")
     void test() throws Exception {
         //given
-        Long userId = 1L;
+        UserPoint userPoint = new UserPoint(1L, 1000L, 1000L);
+        given(pointService.getUserPoint(1L)).willReturn(userPoint);
+
         // when & then
-        mockMvc.perform(get("/point/{id}", userId))
+        mockMvc.perform(get("/point/{id}", 1L))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("id").value(0))
-                .andExpect(jsonPath("point").value(0))
-                .andExpect(jsonPath("updateMillis").value(0))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.point").value(1000L))
+                .andExpect(jsonPath("$.updateMillis").value(1000L))
                 .andDo(print());
     }
 }
